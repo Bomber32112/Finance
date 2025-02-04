0da5960d03903d65d84b0d5dc66179325176ed84
User <bomber32112@gmail.com>
2025-02-05 02:03:26 +1000

    Добавьте файлы проекта.

---
+++ Finance/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IncomeCategory incomeCategory = new IncomeCategory();
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewCategory newCategory = new NewCategory();
+            
+            DataContext = this;
+            newCategory.Close();
+        }
+    }
+    public class Category 
+    {
+        public string Title { get; set; }
+    }
+    public class IncomeCategory : Category 
+    {
+       public ObservableCollection<SomeIncome> Incomes { get; set; } = new();
+    }
+    public class SomeIncome 
+    {
+        public string Name { get; set; }
+
+    }
+}
